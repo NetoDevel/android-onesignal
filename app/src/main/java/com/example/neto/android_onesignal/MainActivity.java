8aaf324b0e0c5d2e1a0852ae818485b64129884f
José Vieira <josevieira.dev@gmail.com>
2017-04-09 17:54:17 -0300

    simple example

---
+++ app/src/main/java/com/example/neto/android_onesignal/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.neto.android_onesignal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OneSignal.startInit(this).init();
+    }
+}
